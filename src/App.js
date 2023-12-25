e7e36f25085e81dbcf80a7405c91944f199657d9
Lakshay <lakshaygoyal020@gmail.com>
2023-12-25 19:51:40 +0530

    Chapter-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  let name = "My App"
+  let className = "App-header"
+  console.log("App")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className={className}>{name}</div>
+      <Demo />
+    </div>
+  );
+}
+
+function Demo() {
+  console.log("Demo")
+  return (
+    <div className='App'>
+      <div className='App-header'>Demo</div>
     </div>
   );
 }
